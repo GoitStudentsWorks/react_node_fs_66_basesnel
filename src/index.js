03d41148853a2626b9a37a07630f65a657d69c75
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-07-24 02:24:31 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={<Loader />} persistor={persistedStore}>
-        <BrowserRouter basename="/project-yummy-f">
+        <BrowserRouter basename="/react_node_fs_66_basesnel">
           <ScrollToTop />
           <App />
         </BrowserRouter>
